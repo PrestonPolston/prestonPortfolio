48101b2cd18d6305ec88a07899298f533dd49170
preston <polston13@outlook.com>
2024-10-02 18:46:44 -0400

    reworking my navBar css added images into an image folder and added fonts

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import gitHub from "../assets/image/gitHub.png";
+import linkedIn from "../assets/image/linkedIn.png";
+import prestonImage from "../assets/image/prestonImage.webp";
 
 function NavBar() {
   const [activeLink, setActiveLink] = useState("home");
@@ -27,53 +30,58 @@ function NavBar() {
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="#home"></Navbar.Brand>
-        <img src="prestonImage.ico" alt="Logo" />
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link
-              href="#home"
-              className={
-                activeLink === "home" ? "activeNavBarLink" : "NavBarLink"
-              }
-              onClick={() => onUpdateActiveLink("home")}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              href="#Skills"
-              className={
-                activeLink === "Skills" ? "activeNavBarLink" : "NavBarLink"
-              }
-              onClick={() => onUpdateActiveLink("Skills")}
-            >
-              Skills
-            </Nav.Link>
-            <Nav.Link
-              href="#Projects"
-              className={
-                activeLink === "Projects" ? "activeNavBarLink" : "NavBarLink"
-              }
-              onClick={() => onUpdateActiveLink("Projects")}
-            >
-              Projects
-            </Nav.Link>
-          </Nav>
-          <span>
-            <div className="socialIcon">
-              <a href="https://github.com/PrestonPolston">
-                <img src="github-mark-white.png" alt="gitHub" />
-              </a>
-              <a href="https://www.linkedin.com/in/prestonpolston/">
-                <img src="In-White-96.png" alt="LinkedIn" />
-              </a>
-            </div>
-            <button className="contact" onClick={() => console.log("click")}>
-              <span>Contact Me</span>
-            </button>
-          </span>
-        </Navbar.Collapse>
+        <div className="navContainer">
+          <Navbar.Brand href="#home">
+            <img className="prestonLogo" src={prestonImage} alt="Logo" />
+          </Navbar.Brand>
+          {/* <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle> */}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="navText">
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "activeNavBarLink" : "NavBarLink"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#Skills"
+                className={
+                  activeLink === "Skills" ? "activeNavBarLink" : "NavBarLink"
+                }
+                onClick={() => onUpdateActiveLink("Skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#Projects"
+                className={
+                  activeLink === "Projects" ? "activeNavBarLink" : "NavBarLink"
+                }
+                onClick={() => onUpdateActiveLink("Projects")}
+              >
+                Projects
+              </Nav.Link>
+            </Nav>
+            {/* <span className="navbar-text">
+              <div className="socialIcon">
+                <a href="https://github.com/PrestonPolston">
+                  <img src={gitHub} alt="gitHub" />
+                </a>
+                <a href="https://www.linkedin.com/in/prestonpolston/">
+                  <img src={linkedIn} alt="LinkedIn" />
+                </a>
+              </div>
+              <button className="vvd" onClick={() => console.log("click")}>
+                <span>Contact Me</span>
+              </button>
+            </span> */}
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
